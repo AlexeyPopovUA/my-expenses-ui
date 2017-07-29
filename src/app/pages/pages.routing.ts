375516a2335b9539ff444db568618a918308b005
AlexeyPopovUA <alexejpopovua@gmail.com>
2017-07-29 21:41:48 +0200

    Manual update

--- src/app/pages/pages.routing.ts
+++ src/app/pages/pages.routing.ts
@@ -1,6 +1,6 @@
-import {Routes, RouterModule}  from '@angular/router';
-import {Pages} from './pages.component';
-import {ModuleWithProviders} from '@angular/core';
+import { Routes, RouterModule }  from '@angular/router';
+import { Pages } from './pages.component';
+import { ModuleWithProviders } from '@angular/core';
 // noinspection TypeScriptValidateTypes
 
 // export function loadChildren(path) { return System.import(path); };
