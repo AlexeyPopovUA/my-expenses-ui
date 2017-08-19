e103509d2ef12a4ca7d1bb6478cd17f2a93b2199
AlexeyPopovUA <alexejpopovua@gmail.com>
2017-08-19 19:16:35 +0200

    Trying to implement remote grid editing feature

--- src/app/pages/browse/browse-table/browse.table.component.ts
+++ src/app/pages/browse/browse-table/browse.table.component.ts
@@ -1,37 +1,74 @@
 import {Component} from '@angular/core';
 import {Http} from '@angular/http';
 
-import {ServerDataSource} from 'ng2-smart-table';
+import {BrowseTableDataSource} from './browse.table.data.source';
 
 @Component({
   selector: 'browse-table',
   template: `
-    <ng2-smart-table [settings]="settings" [source]="source"></ng2-smart-table>
+    <ng2-smart-table
+      [settings]="settings"
+      [source]="source"
+      (deleteConfirm)="onDeleteConfirm($event)"
+    ></ng2-smart-table>
   `,
 })
 export class BrowseTableComponent {
 
   settings = {
-    columns: {
-      id: {
-        title: 'ID',
-      },
-      albumId: {
-        title: 'Album',
-      },
-      title: {
-        title: 'Title',
-      },
-      url: {
-        title: 'Url',
-      },
+    pager: {
+      display: true,
+      perPage: 50
     },
+    columns: {
+      name: {
+        title: 'name',
+        filter: false
+      },
+      category: {
+        title: 'category',
+        filter: false
+      },
+      date: {
+        title: 'date',
+        filter: false
+      },
+      value: {
+        title: 'value',
+        filter: false
+      }
+    }
   };
 
-  source: ServerDataSource;
+  source: BrowseTableDataSource;
 
   constructor(http: Http) {
-    console.warn("constructor");
-    this.source = new ServerDataSource(http, {endPoint: 'https://jsonplaceholder.typicode.com/photos'});
+    this.source = new BrowseTableDataSource(http, {
+      endPoint: 'http://localhost:3000/payments/get',
+      dataKey: 'items',
+      pagerPageKey: 'page',
+      pagerLimitKey: 'limit',
+      totalKey: 'results',
+      sortFieldKey: 'sort',
+      sortDirKey: 'direction'
+    });
+
+    this.source.onUpdated().subscribe(value => {
+      this.onEdited(value);
+    });
+  }
+
+  onDeleteConfirm(event) {
+    if (window.confirm('Are you sure you want to delete?')) {
+      event.confirm.resolve();
+    } else {
+      event.confirm.reject();
+    }
+  }
+
+  onEdited(value) {
+    console.warn('onEdited', value);
+    //event.confirm.resolve();
+    //return Promise.resolve(value);
   }
 }
---
+++ src/app/pages/browse/browse-table/browse.table.data.source.ts
@@ -0,0 +1,13 @@
+import {ServerDataSource} from "ng2-smart-table";
+
+export class BrowseTableDataSource extends ServerDataSource {
+  find(element: any): Promise<any> {
+    const found = this.data.find(el => el._id === element._id);
+
+    if (found) {
+      return Promise.resolve(found);
+    }
+
+    return Promise.reject(new Error('Element was not found in the dataset'));
+  }
+}
